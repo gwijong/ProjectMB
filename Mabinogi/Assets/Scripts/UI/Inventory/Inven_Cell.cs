f19590ec7380852cc94a30045b3f7bdd6ac39708
gwijong <drophail@daum.net>
2022-05-02 15:26:26 +0900

    20220502

---
+++ Mabinogi/Assets/Scripts/UI/Inventory/Inven_Cell.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Inven_Cell : MonoBehaviour
+{
+    public GameObject cell;
+    int ypos = 0;
+    void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                GameObject currentCell = Instantiate(cell) ;
+                Vector3 pos = new Vector3(currentCell.transform.position.x + (48 * i), currentCell.transform.position.y + ypos , currentCell.transform.position.z);
+                currentCell.GetComponent<RectTransform>().position = pos;
+            }
+            ypos = ypos + 48;
+        }
+    }
+
+}
